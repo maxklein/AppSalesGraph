4fdfe93cd0cfe99d1ccd35c4ead35833daa366aa
Max Klein <maximusklein@gmail.com>
2010-01-27 18:54:34 +0100

    Changed License to LGPL

--- setup_win.py
+++ setup_win.py
@@ -1,3 +1,11 @@
+# AppSalesGraph: AppStore Sales Graphing
+# Copyright (c) 2010 by Max Klein (maximusklein@gmail.com)
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+
 from setuptools import setup
 
 from distutils.core import setup
